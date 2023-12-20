273b6bdf3e02d874b8cdd58ffd086f08f0ecd96f
Supakit <book0970403815@hotmail.com>
2023-12-20 15:34:23 +0700

    feat : send assignment by book

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import "./App.css";
+import { Button } from "./components/Button";
+import { Alert } from "./components/Alert";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
 function App() {
   return (
     <div className="App">
       <div className="button-components-section">
-        {/* Render ตัว Button 2 แบบ */}
+        <h1>Primary Button</h1>
+        <Button type="primary-button" />
       </div>
       <hr />
       <div className="alert-components-section">
-        {/* Render ตัว Alert 4 แบบ */}
+        <h1>Secondary Button</h1>
+        <Button type="secondary-button" />
+      </div>
+      <div className="all-alert">
+        <Alert type="default" childen="This is error alert box" />
+        <Alert type="hover" childen="This is warning alert box" />
+        <Alert type="error" childen="This is info alert box" />
+        <Alert type="focus" childen="This is success alert box" />
       </div>
     </div>
   );
--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,114 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import iconsmile from "../assets/icon-smile.svg";
+import alerttriangle from "../assets/alert-triangle.svg";
+import checkcircle from "../assets/check-circle.svg";
+import alartcircle from "../assets/alert-circle.svg";
 
-function Alert() {}
+export function Alert(props) {
+  if (props.type === "default") {
+    return (
+      <div>
+        <h3
+          css={css`
+            width: 650px;
+            height: 76px;
+            border-radius: 10px;
+            background: #f9c8c8;
+            display: flex;
+            flex-direction: flex-start;
+            align-items: center;
+            color: #444;
+          `}
+        >
+          <img
+            src={iconsmile}
+            alt="icon-smile"
+            css={css`
+              padding: 0 15px 0 20px;
+            `}
+          />
+          {props.childen}
+        </h3>
+      </div>
+    );
+  } else if (props.type === "hover") {
+    return (
+      <div>
+        <h3
+          css={css`
+            width: 650px;
+            height: 76px;
+            border-radius: 10px;
+            background: #f9d9c8;
+            display: flex;
+            flex-direction: flex-start;
+            align-items: center;
+            color: #444;
+          `}
+        >
+          <img
+            src={alerttriangle}
+            alt="icon-smile"
+            css={css`
+              padding: 0 15px 0 20px;
+            `}
+          />
+          {props.childen}
+        </h3>
+      </div>
+    );
+  } else if (props.type === "error") {
+    return (
+      <div>
+        <h3
+          css={css`
+            width: 650px;
+            height: 76px;
+            border-radius: 10px;
+            background: #f9ebc8;
+            display: flex;
+            flex-direction: flex-start;
+            align-items: center;
+            color: #444;
+          `}
+        >
+          <img
+            src={alartcircle}
+            alt="icon-smile"
+            css={css`
+              padding: 0 15px 0 20px;
+            `}
+          />
+          {props.childen}
+        </h3>
+      </div>
+    );
+  } else if (props.type === "focus") {
+    return (
+      <div>
+        <h3
+          css={css`
+            width: 650px;
+            height: 76px;
+            border-radius: 10px;
+            background: #cef7cd;
+            display: flex;
+            flex-direction: flex-start;
+            align-items: center;
+            color: #444;
+          `}
+        >
+          <img
+            src={checkcircle}
+            alt="icon-smile"
+            css={css`
+              padding: 0 15px 0 20px;
+            `}
+          />
+          {props.childen}
+        </h3>
+      </div>
+    );
+  }
+}
